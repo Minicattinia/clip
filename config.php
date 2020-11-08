5546b333431b000a2786314a108b43dcd1660044
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2020-11-08 18:15:03 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+//xxx Wajib Diisi
+
+$token = "ODI4MWI5NGQtOGJmOC00OWEwLWE1MGYtZjIwZTUyY2Y1YjFm";
+$uuid = "7245309c-6d51-4357-b546-4414edb40367";
+$userid = "34001561";
+
+$eksternal_version = "2.0.2";
+$version = "42";
+$device = "9";
+$timezone = "7";
+$lang = "id";
+
+$spin_version = "40";
+$user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"; //Search in Google Chrome With Keyword "my user agent"
